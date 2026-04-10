61c355d7dee0a569aa3165830e24b6e5206126f8
Ivan Jambresic <ijambres.math@pmf.hr>
2026-04-10 21:53:11 +0200

    database fix

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ const path = require('path');
 const Database = require('better-sqlite3');
 
 const dbPath = path.join(__dirname, '..', 'db', 'data.sqlite');
-const db = new Database(dbPath);
+const Database = require('better-sqlite3');
+const db = new Database('/data/app.db');
 db.pragma('journal_mode = WAL');
 
 module.exports = db;
