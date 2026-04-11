e4d35b099ee41a1d27471b9e271df46cfdfb6028
Ivan Jambresic <ijambres.math@pmf.hr>
2026-04-11 18:29:19 +0200

    sad ce na dobru bazu

--- src/config/db.js
+++ src/config/db.js
@@ -10,14 +10,16 @@ if (isProduction) {
     ssl: { rejectUnauthorized: false }
   });
 } else {
-  pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'a',
-    port: 5432,
-    ssl: false
-  });
+  const pool = new Pool({
+  user: 'postgres',
+  host: 'metro.proxy.rlwy.net',
+  database: 'railway',        
+  password: 'DBXKhVifdfxIebVUGRWHltihZvdUcxxR',
+  port: 29137,                
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 }
 
 module.exports = pool;
