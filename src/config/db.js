64767cbb0b5294d711f0990ca748194ef2464e8e
Ivan Jambresic <ijambres.math@pmf.hr>
2026-04-11 18:32:10 +0200

    spajaj uvijek na istu bazu

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,6 @@
 const { Pool } = require('pg');
 
-
-const isProduction = process.env.NODE_ENV === 'production';
-let pool;
-
-if (isProduction) {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-  });
-} else {
-  const pool = new Pool({
+const pool = new Pool({
   user: 'postgres',
   host: 'metro.proxy.rlwy.net',
   database: 'railway',        
@@ -20,6 +10,5 @@ if (isProduction) {
     rejectUnauthorized: false
   }
 });
-}
 
 module.exports = pool;
